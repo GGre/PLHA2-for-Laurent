44000a814996aec6c373539eb0bf709816a7d0ae
Georges Gressot <29573578+GGre@users.noreply.github.com>
2018-02-06 16:08:06 +0100

    Update plha2.py

--- plha2.py
+++ plha2.py
@@ -43,11 +43,11 @@ print(j, ' file(s) remained in place.')
 
 
 input("Press 'Entry key' to close this program...")
-# For linux use, cancel the eventual # at the beginning of the upper line
+# For linux use, cancel the eventual # at the beginning of the upper line (beginning with input)
 # and put a # (if not yet) at the beginning of the following one.
 
 #os.system("pause")
 
 # For Windows use, cancel the eventual # at the beginning of the just upper
-# line and put a # (if not yet) at the beginning of the upper line beginning
+# line (beginning with os.system) and put a # (if not yet) at the beginning of the upper line beginning
 # with : input("Press 'Entry key' to ...")
